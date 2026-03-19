71280aa889aa09b55e6c4afc91dda8765a67f445
mesum-exe <mesum.a680@gmail.com>
2026-03-19 11:07:59 +0400

    Created the JS file with getComputerChoice() logic
    The logic uses the Math.random() function to output a random number
    between 0 and 1, which is multiplied by 3 to increase the range
    to between 0 and 3.
    
    Using Math.floor() we make sure all possible choices made by the
    computer are either 0, 1, or 2.
    
    Using conditional statements, 0 was denoted for Rock, 1 for Scissors,
    and 2 for Paper. These are logged to the console.

---
+++ script.js
@@ -0,0 +1,22 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()*3)
+    // 0, 0.3, 0.6, 0.9, 1.2, 1.5, 1.8, 2.1, 2.4, 2.7
+    // --> 0, 1, 2
+    // Equal probability for each choice
+
+    if (choice === 0) {
+        console.log("rock")
+    }
+    else if (choice === 1) {
+        console.log("paper")
+    }
+    else if (choice === 2) {
+        console.log("scissors")
+    }    
+    else {
+        console.log("Error. Start again.")
+    }
+
+    return;
+
+}
